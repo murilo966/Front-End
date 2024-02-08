389efa75b590ed8667bea26ff5420f59c4e398d0
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-02-07 22:12:20 -0300

    apagando comentários

--- src/pages/requisicoesPost.tsx
+++ src/pages/requisicoesPost.tsx
@@ -24,7 +24,6 @@ function RequisicoesPost(){
 
     const carregarUsuarios = async () => {
         try{
-            //aqui podemos escolher entre um ou vários produtos, basta adicionar /id (exemplo: 5)
             let response = await fetch("https://jsonplaceholder.typicode.com/todos/5")
             let Json = await response.json();
             
@@ -41,37 +40,25 @@ function RequisicoesPost(){
     }, [])
 
     const handleAddClick = async () => {
-        // Testando se funcionou a criação dos campos e chamada de eventos.
-        //alert( addTitleText + ' - ' + addBodyText);
- 
-        //Testes de validação se os dados estão preenchidos.
         if (addTitleText && addBodyText) {
  
-            // Para o POST, a requisição é a mesma, via FETCH, no entanto, exite alguns parametros a mais.
             let response = await fetch('https://jsonplaceholder.typicode.com/posts', 
                                         {
-                                            // Por padrão, o method do fetch, é o GET, por isso não precisamos especificar.
                                             method: 'POST',
                                             body: JSON.stringify
                                             ({
-                                                // campos requisitados pela API.
                                                 title: addTitleText,
                                                 body: addBodyText,
                                                 userID: 1
                                             }),
                                             headers: {
-                                                // Caso seja necessário o envio de algum parametro no cabeçalho da requisição, se faz aqui.
                                                 'Content-Type': 'application/json'
                                             }                                        
                                         }
-                                      );
-            // Coleta o retorno do meu metodo post.                          
+                                      );                          
             let json = await response.json();
-            
-            // Exibe no log do console.
             console.log(json);
- 
-            // Após inserir as informações, posso inserir o resultado na minha lista.
+
             if (json.id){
                 alert('Post Adicionado com sucesso!')     
                 setUsuarios((usuarios) => [...usuarios, json] );
